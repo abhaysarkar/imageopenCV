9889af966aabc49b9b5c779d036ef213baf8b0c7
abhaysarkar <abhaysheets@gmail.com>
2024-09-10 17:41:00 +0530

    Update OCRController.java

--- src/main/java/in/dataman/controller/OCRController.java
+++ src/main/java/in/dataman/controller/OCRController.java
@@ -46,7 +46,7 @@ public class OCRController {
     
     @PostConstruct
     public void init() throws IOException {
-        String herokuTessdataPath = "/app/vendor/tesseract-ocr/share/tessdata";  // Path for Heroku
+        String herokuTessdataPath = "/app/vendor/tesseract-ocr/tessdata";  // Path for Heroku
 
         // Check if the app is running on Heroku by checking for a specific environment variable
         String dyno = System.getenv("DYNO");
